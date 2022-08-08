f6d364c7b4c74edc0087cf0ad42b61c79e817325
mmoraisjr <jr.morais.marcos@gmail.com>
2022-08-08 19:50:38 -0300

    update function atualizaEstatistica

--- js/main.js
+++ js/main.js
@@ -1,19 +1,59 @@
 const controle = document.querySelectorAll("[data-controle]");
+const estatisticas = document.querySelectorAll("[data-estatistica]");
+const pecas = {
+  bracos: {
+    forca: 29,
+    poder: 35,
+    energia: -21,
+    velocidade: -5,
+  },
 
-controle.forEach( (elemento) => {
-    elemento.addEventListener("click",
-        (e) => {
-            controlaNivel(e.target.dataset.controle, e.target.parentNode);
-        })
+  blindagem: {
+    forca: 41,
+    poder: 20,
+    energia: 0,
+    velocidade: -20,
+  },
+  nucleos: {
+    forca: 0,
+    poder: 7,
+    energia: 48,
+    velocidade: -24,
+  },
+  pernas: {
+    forca: 27,
+    poder: 21,
+    energia: -32,
+    velocidade: 42,
+  },
+  foguetes: {
+    forca: 0,
+    poder: 28,
+    energia: 0,
+    velocidade: -2,
+  },
+};
+
+controle.forEach((elemento) => {
+  elemento.addEventListener("click", (e) => {
+    controlaNivel(e.target.dataset.controle, e.target.parentNode);
+    atualizaEstatistica(e.target.dataset.peca);
+  });
 });
 
 function controlaNivel(operacao, componente) {
-    const peca = componente.querySelector("[data-contador]");
+  const peca = componente.querySelector("[data-contador]");
 
-    if (operacao === "-") {
-        peca.value = parseInt(peca.value) - 1;
+  if (operacao === "-") {
+    peca.value = parseInt(peca.value) - 1;
+  } else {
+    peca.value = parseInt(peca.value) + 1;
+  }
+}
 
-    } else {
-        peca.value = parseInt(peca.value) + 1;
-    }
+function atualizaEstatistica(peca) {
+  estatisticas.forEach((e) => {
+    e.textContent =
+      parseInt(e.textContent) + pecas[peca][e.dataset.estatistica];
+  });
 }
